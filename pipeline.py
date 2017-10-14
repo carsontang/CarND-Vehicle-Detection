e9385eb0d1f9d28c69f9604937262f5c46a9265c
Carson Tang <ctang@dhcp-172-20-174-117.dhcp.thefacebook.com>
2017-10-13 20:52:33 -0700

    optimize code

--- pipeline.py
+++ pipeline.py
@@ -87,11 +87,6 @@ def extract_features(images, color_space='RGB', spatial_size=(32, 32),
     2) histogram of pixel intensities
     3) histogram of gradients
     """
-#
-# # def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
-#                         hist_bins=32, orient=9,
-#                         pix_per_cell=8, cell_per_block=2, hog_channel=0,
-#                         use_spatial_feat=True, use_hist_feat=True, use_hog_feat=True):
 
     features = []
     for img in tqdm(images):
@@ -99,47 +94,7 @@ def extract_features(images, color_space='RGB', spatial_size=(32, 32),
                                            orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
                                            hog_channel=hog_channel, use_spatial_feat=use_spatial_feat,
                                            use_hist_feat=use_hist_feat, use_hog_feat=use_hog_feat)
-
         features.append(img_features)
-        # image_features = []
-        # if color_space != 'RGB':
-        #     if color_space == 'HSV':
-        #         feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        #     elif color_space == 'LUV':
-        #         feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        #     elif color_space == 'HLS':
-        #         feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        #     elif color_space == 'YUV':
-        #         feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        #     elif color_space == 'YCrCb':
-        #         feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-        #     elif color_space == 'GRAY':
-        #         feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        #         feature_image = np.stack((feature_image, feature_image, feature_image), axis=2)
-        # else:
-        #     feature_image = np.copy(img)
-        #
-        # if use_spatial_feat:
-        #     spatial_features = bin_spatial(feature_image, size=spatial_size)
-        #     image_features.append(spatial_features)
-        # if use_hist_feat:
-        #     hist_features = color_hist(feature_image, nbins=hist_bins)
-        #     image_features.append(hist_features)
-        # if use_hog_feat:
-        #     if hog_channel == 'ALL':
-        #         hog_features = []
-        #         for channel in range(feature_image.shape[2]):
-        #             hog_features.append(get_hog_features(feature_image[:,:,channel],
-        #                                 orient, pix_per_cell, cell_per_block,
-        #                                 vis=False, feature_vec=True))
-        #         hog_features = np.ravel(hog_features)
-        #     else:
-        #         hog_features = get_hog_features(feature_image[:,:,hog_channel], orient,
-        #                     pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-        #     image_features.append(hog_features)
-
-        # Combine all of the collected features for a single image
-        # features.append(np.concatenate(img_features))
     return features
 
 def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
@@ -220,20 +175,14 @@ def train(cars, notcars, svc, X_scaler):
     print('Using:',orient,'orientations',pix_per_cell,
         'pixels per cell and', cell_per_block,'cells per block')
     print('Feature vector length:', len(X_train[0]))
-    # Use a linear SVC
-    #svc = LinearSVC()
     # Check the training time for the SVC
     t=time.time()
     svc.fit(X_train, y_train)
     t2 = time.time()
-    print(round(t2-t, 2), 'Seconds to train SVC...')
+    print(round(t2-t, 2), 'seconds to train SVC...')
     # Check the score of the SVC
     print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
 
-# Define a function that takes an image,
-# start and stop positions in both x and y,
-# window size (x and y dimensions),
-# and overlap fraction (for both x and y)
 def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
                     xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
     # If x and/or y start/stop positions not defined, set to image size
@@ -257,9 +206,6 @@ def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
     # Initialize a list to append window positions to
     window_list = []
     # Loop through finding x and y window positions
-    # Note: you could vectorize this step, but in practice
-    # you'll be considering windows one by one with your
-    # classifier, so looping makes sense
     for ys in range(ny_windows):
         for xs in range(nx_windows):
             # Calculate window position
@@ -274,8 +220,6 @@ def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
     return window_list
 
 
-# Define a function you will pass an image
-# and the list of windows to be searched (output of slide_windows())
 def search_windows(img, windows, clf, scaler, color_space='RGB',
                     spatial_size=(32, 32), hist_bins=32,
                     hist_range=(0, 256), orient=9,
@@ -307,7 +251,6 @@ def search_windows(img, windows, clf, scaler, color_space='RGB',
     return on_windows
 
 
-# Define a function to draw bounding boxes
 def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     # Make a copy of the image
     imcopy = np.copy(img)
@@ -339,42 +282,55 @@ def apply_threshold(heatmap, threshold):
     return heatmap
 
 
-# Draw bounding boxes based on labels
+# not done
 def draw_labeled_bboxes(img, labels):
     # Iterate through all detected cars
     for car_number in range(1, labels[1]+1):
         # Find pixels with each car_number label value
         nonzero = (labels[0] == car_number).nonzero()
-
         # Identify x and y values of those pixels
         nonzeroy = np.array(nonzero[0])
         nonzerox = np.array(nonzero[1])
-
         # Define a bounding box based on min/max x and y
         bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
-        bbox_w = (bbox[1][0] - bbox[0][0])
-        bbox_h = (bbox[1][1] - bbox[0][1])
-
-        # Filter final detections for aspect ratios, e.g. thin vertical box is likely not a car
-        aspect_ratio = bbox_w / bbox_h  # width / height
-        #print('ar: %s' % (aspect_ratio,))
-
-        # Also if small box "close" to the car (i.e. bounding box y location is high),
-        # then probaby not a car
-        bbox_area = bbox_w * bbox_h
-
-        if bbox_area < small_bbox_area and bbox[0][1] > close_y_thresh:
-            small_box_close = True
-        else:
-            small_box_close = False
-
-        # Combine above filters with minimum bbox area filter
-        if aspect_ratio > min_ar and aspect_ratio < max_ar and not small_box_close and bbox_area > min_bbox_area:
-            # Draw the box on the image
-            cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
-
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
     # Return the image
     return img
+# def draw_labeled_bboxes(img, labels):
+#     # Iterate through all detected cars
+#     for car_number in range(1, labels[1]+1):
+#         # Find pixels with each car_number label value
+#         nonzero = (labels[0] == car_number).nonzero()
+#
+#         # Identify x and y values of those pixels
+#         nonzeroy = np.array(nonzero[0])
+#         nonzerox = np.array(nonzero[1])
+#
+#         # Define a bounding box based on min/max x and y
+#         bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+#         bbox_width = (bbox[1][0] - bbox[0][0])
+#         bbox_height = (bbox[1][1] - bbox[0][1])
+#
+#         # Use the ratio of width : height to filter out thin bounding boxes
+#         ratio = bbox_width / bbox_height
+#
+#         # Also if small box "close" to the car (i.e. bounding box y location is high),
+#         # then probaby not a car
+#         bbox_area = bbox_width * bbox_height
+#
+#         if bbox_area < small_bbox_area and bbox[0][1] > close_y_thresh:
+#             small_box_close = True
+#         else:
+#             small_box_close = False
+#
+#         # Combine above filters with minimum bbox area filter
+#         if ratio > min_ar and ratio < max_ar and not small_box_close and bbox_area > min_bbox_area:
+#             # Draw the box on the image
+#             cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+#
+#     # Return the image
+#     return img
 
 def numpyify(pattern):
     arrs = []
@@ -388,21 +344,21 @@ def run_windows():
     X_scaler = StandardScaler()
 
     if global_settings.use_pretrained:
+        print("Using pretrained model...")
         with open('model.p', 'rb') as f:
-            save_dict = pickle.load(f)
-        svc = save_dict['svc']
-        X_scaler = save_dict['X_scaler']
-
+            d = pickle.load(f)
+        svc = d['svc']
+        X_scaler = d['X_scaler']
         print('Loaded pre-trained model from model.p')
     else:
-        print('Reading training data and training classifier from scratch')
-
+        print('Loading dataset of cars and non-cars')
         cars = numpyify('dataset/vehicles/*/*')
         notcars = numpyify('dataset/non-vehicles/*/*')
+
+        print('Training classifier')
         train(cars, notcars, svc, X_scaler)
 
-        print('Training complete, saving trained model to model.p')
-
+        print('Training completed, and serializing trained model')
         with open('model.p', 'wb') as f:
             pickle.dump({'svc': svc, 'X_scaler': X_scaler}, f)
 
@@ -412,8 +368,6 @@ def run_windows():
     image = mpimg.imread(image_file)
     draw_image = np.copy(image)
 
-    windows = slide_window(image, x_start_stop=(0, 1280), y_start_stop=(500, 700),
-                           xy_window=(128, 128), xy_overlap=(global_settings.pct_overlap, global_settings.pct_overlap))
     windows = slide_window(image, x_start_stop=(100, 1180), y_start_stop=(400, 500),
                            xy_window=(96, 96), xy_overlap=(pct_overlap, pct_overlap))
 
